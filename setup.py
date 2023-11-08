2054e3c7561bbae1673e75bc99ec9e251e1f5de5
javiermutr <javiermutr@gmail.com>
2023-11-08 16:40:55 -0500

    Inclusion of setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name='math_quiz',
+    version='1.0',
+    packages=[''],
+    install_requires=[
+        'numpy',
+        'unittest',
+    ],
+    author='Javier Munoz',
+    author_email='javier.munoz@fau.de',
+    description='This package was created for a academic homework in DSSS subject',
+    url='https://github.com/javiermutr/DSSS',
+)
